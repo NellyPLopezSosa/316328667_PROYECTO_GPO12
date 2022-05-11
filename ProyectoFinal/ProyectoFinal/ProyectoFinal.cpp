743f558c05e95ca1ca901b44f9c5ca03214aa942
NellyPLopezSosa <42351383+NellyPLopezSosa@users.noreply.github.com>
2022-05-11 00:08:45 -0500

    Avances 10/05/22
    Documentación

--- ProyectoFinal/ProyectoFinal/ProyectoFinal.cpp
+++ ProyectoFinal/ProyectoFinal/ProyectoFinal.cpp
@@ -1,3 +1,16 @@
+/*
+=================================
+        PROYECTO FINAL
+        316328667
+        GRUPO:12
+=================================
+*/
+
+/*
+>> Blibliotecas
+>> Libraries
+*/
+
 // Std. Includes
 #include <string>
 
@@ -6,7 +19,6 @@
 
 // GLFW
 #include <GLFW/glfw3.h>
-
 
 // Other Libs
 #include "SOIL2/SOIL2.h"
@@ -22,38 +34,59 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+
+// Constantes de la ventana
 // Properties
 const GLuint WIDTH = 800, HEIGHT = 600;
 int SCREEN_WIDTH, SCREEN_HEIGHT;
 
+// Funciones de movimientos
 // Function prototypes
 void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
 void MouseCallback(GLFWwindow* window, double xPos, double yPos);
 void DoMovement();
 
-
-// Camera
+// Propiedades de la cámara
+// Camera properties
 Camera camera(glm::vec3(30.0f, 10.0f, 15.0f));
 bool keys[1024];
 GLfloat lastX = 400, lastY = 300;
 bool firstMouse = true;
 
+/*
+>> Variables globales
+>> Global variables
+*/
+
 GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
 float rot = 0.0f;
+
+/*
+>> Variables para animación
+>> Animation variables
+*/
+
 //Variables animación puerta principal
+//Main door
 float trans = 0.0f;
 bool anim = true;
 bool anim2 = true;
+
 //Variables animación puerta interior
+//Room's door
 float trans2 = 0.0f;
 bool anim3 = true;
 bool anim4 = true;
+
 //Variables animación cajon armario
+//Drawer
 float trans3 = 0.0f;
 bool anim5 = true;
 bool anim6 = true;
+
 //Variables animación puertas armario
+//Closet door
 float trans4 = 0.0f;
 bool anim7 = true;
 bool anim8 = true;
@@ -62,8 +95,14 @@ float tiempo;
 
 int main()
 {
+
+/*
+>> Configuración inicial de la ventana
+>> Initial configuration of window 
+*/
     // Init GLFW
     glfwInit();
+    // Verificación de la compatibilidad, se trabaja con la versión 3.3
     // Set all the required options for GLFW
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -71,9 +110,12 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
+    // Creación de la ventana con los valores definidos
     // Create a GLFWwindow object that we can use for GLFW's functions
     GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Proyecto Final", nullptr, nullptr);
 
+
+    // Verificación de errores de creación de ventana
     if (nullptr == window)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -83,18 +125,16 @@ int main()
     }
 
     glfwMakeContextCurrent(window);
-
     glfwGetFramebufferSize(window, &SCREEN_WIDTH, &SCREEN_HEIGHT);
 
     // Set the required callback functions
     glfwSetKeyCallback(window, KeyCallback);
     glfwSetCursorPosCallback(window, MouseCallback);
 
-    // GLFW Options
-    //glfwSetInputMode( window, GLFW_CURSOR, GLFW_CURSOR_DISABLED );
-
     // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
     glewExperimental = GL_TRUE;
+
+    // Verificación de errores de inicialización de glew
     // Initialize GLEW to setup the OpenGL Function pointers
     if (GLEW_OK != glewInit())
     {
@@ -102,13 +142,17 @@ int main()
         return EXIT_FAILURE;
     }
 
+    // Define las dimensiones del viwport
     // Define the viewport dimensions
     glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
     // OpenGL options
     glEnable(GL_DEPTH_TEST);
 
-    // Setup and compile our shaders
+/*
+>> Configuración y compilación de shaders
+>> Setup and compile of shaders
+*/
     Shader shader("Shaders/modelLoading.vs", "Shaders/modelLoading.frag");
     Shader lampshader("Shaders/lamp.vs", "Shaders/lamp.frag");
     Shader Anim("Shaders/anim.vs", "Shaders/anim.frag");
@@ -120,6 +164,7 @@ int main()
     // Load models
     glm::mat4 projection = glm::perspective(camera.GetZoom(), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
 
+    //Definición de vértices
     GLfloat vertices[] =
     {
         // positions          // colors           // texture coords
@@ -127,16 +172,22 @@ int main()
      0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
     -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
     -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
-
     };
 
+    //Definición de índices
     GLuint indices[] =
     {  // Note that we start from 0!
         0,1,3,
         1,2,3
-
     };
 
+/*
+>> Configuración de VBO, VAO y EBO
+>> Setup of VBO, VAO and EBO
+
+VBO: Contiene la información acerca de los vértices
+VAO: Objeto que contiene uno o más VBO
+*/
     // First, set the container's VAO (and VBO)
     GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
@@ -160,7 +211,13 @@ int main()
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
 
-    // Load models
+/*
+>> Carga de los modelos
+>> Load models
+
+Define el nombre de los modelos, y se localiza los objetos con extención .obj dentro de su carpeta correspondiente.
+*/
+
     Model Casa((char*)"Models/Casa/Casa.obj");
     Model PuertaEntrada((char*)"Models/Casa/PuertaEntrada.obj");
     Model PuertaInterna((char*)"Models/Casa/PuertaInterna.obj");
@@ -178,7 +235,10 @@ int main()
     Model Caldo((char*)"Models/Olla/Caldo.obj");
     Model Ingredientes((char*)"Models/Olla/Ingredientes.obj");
 
-    // Load textures
+/*
+>> Carga de texturas
+>> Load textures
+*/
     GLuint texture;
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
@@ -203,8 +263,9 @@ int main()
         glfwPollEvents();
         DoMovement();
 
+        // Color de la ventana (recibe colores normalizados)
         // Clear the colorbuffer
-        glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+        glClearColor(0.172f, 0.235f, 0.164f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         shader.Use();
@@ -213,16 +274,24 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
 
-        // Draw the loaded model
 
+/*
+>> Se dibujan los modelos cargados
+>> Draw the loaded model
+
+Estos objetos emplean a shader
+*/
+        // Seteo de la matriz
         glm::mat4 model(1);
 
+        // Se dibuja el objeto Casa
         model = glm::mat4(1);
         model = glm::rotate(model, glm::radians(-rot), glm::vec3(0.0f, 1.0f, 0.0f));
         model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         Casa.Draw(shader);
 
+        // Se dibuja el objeto PuertaEntrada
         model = glm::mat4(1);
         model = glm::rotate(model, glm::radians(-rot), glm::vec3(0.0f, 1.0f, 0.0f));
         model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));
@@ -231,6 +300,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         PuertaEntrada.Draw(shader);
 
+        // Se dibuja el objeto PuertaInterna
         model = glm::mat4(1);
         model = glm::rotate(model, glm::radians(-rot), glm::vec3(0.0f, 1.0f, 0.0f));
         model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));
@@ -239,6 +309,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         PuertaInterna.Draw(shader);
         
+        // Se dibuja el objeto Tatami
         model = glm::mat4(1);
         model = glm::rotate(model, glm::radians(rot), glm::vec3(0.0f, 1.0f, 0.0f));
         model = glm::translate(model, glm::vec3(8.0f, 2.1f, 8.0f));
@@ -246,6 +317,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         Tatami.Draw(shader);
 
+        // Se dibuja el objeto Tatami
         model = glm::mat4(1);
         model = glm::rotate(model, glm::radians(rot), glm::vec3(0.0f, 1.0f, 0.0f));
         model = glm::translate(model, glm::vec3(8.0f, 2.1f, -8.0f));
@@ -253,6 +325,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         Tatami.Draw(shader);
 
+        // Se dibuja el objeto Jarron
         model = glm::mat4(1);
         model = glm::rotate(model, glm::radians(-rot), glm::vec3(0.0f, 1.0f, 0.0f));
         model = glm::translate(model, glm::vec3(18.0f, 0.0f, 3.0f));
@@ -260,6 +333,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         Jarron.Draw(shader);
 
+        // Se dibuja el objeto Cajonera
         model = glm::mat4(1);
         model = glm::rotate(model, glm::radians(-rot), glm::vec3(0.0f, 0.5f, 0.0f));
         model = glm::translate(model, glm::vec3(3.5f, 2.64f, 17.4f));
@@ -267,6 +341,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         Cajonera.Draw(shader);
 
+        // Se dibuja el objeto Bambu
         model = glm::mat4(1);
         model = glm::rotate(model, glm::radians(-rot), glm::vec3(0.0f, 1.0f, 0.0f));
         model = glm::translate(model, glm::vec3(9.5f, 6.8f, 0.0f));
@@ -274,6 +349,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         Bambu.Draw(shader);
 
+        // Se dibuja el objeto Olla
         model = glm::mat4(1);
         model = glm::rotate(model, glm::radians(-rot), glm::vec3(0.0f, 1.0f, 0.0f));
         model = glm::translate(model, glm::vec3(9.44f, 3.54f, 0.0f));
@@ -281,6 +357,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         Olla.Draw(shader);
 
+        // Se dibuja el objeto Lampara
         model = glm::mat4(1);
         model = glm::rotate(model, glm::radians(-rot), glm::vec3(0.0f, 1.0f, 0.0f));
         model = glm::translate(model, glm::vec3(3.0f, 2.0f, -9.0f));
@@ -288,6 +365,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         Lampara.Draw(shader);
 
+        // Se dibuja el objeto Armario
         model = glm::mat4(1);
         model = glm::translate(model, glm::vec3(4.0f, 2.2f, -17.6f));
         model = glm::scale(model, glm::vec3(0.6f, 0.6f, 0.6f));
@@ -295,6 +373,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         Armario.Draw(shader);
 
+        // Se dibuja el objeto CajonArmario
         model = glm::mat4(1);
         model = glm::translate(model, glm::vec3(4.0f, 2.2f, -17.6f));
         model = glm::scale(model, glm::vec3(0.6f, 0.6f, 0.6f));
@@ -303,6 +382,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         CajonArmario.Draw(shader);
 
+        // Se dibuja el objeto PuertasArmario
         model = glm::mat4(1);
         model = glm::translate(model, glm::vec3(4.0f, 2.2f, -17.6f));
         model = glm::scale(model, glm::vec3(0.6f, 0.6f, 0.6f));
@@ -311,6 +391,7 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         PuertasArmario.Draw(shader);
 
+        // Se dibuja el objeto Fogata
         model = glm::mat4(1);
         model = glm::rotate(model, glm::radians(-rot), glm::vec3(0.0f, 1.0f, 0.0f));
         model = glm::translate(model, glm::vec3(9.5f, 1.3f, -0.4f));
@@ -320,18 +401,29 @@ int main()
 
         glBindVertexArray(0);
 
-        //Animación del fuego
+/*
+>> Generación de animaciones complejas con uso de los shaders "lightingShader" y "Anim" correspondientes a cada objeto
+>> Generation of complex animations using the shaders "lightingShader" and "Anim" corresponding to each object
+*/
+
+        // Configuración del lightingShader
+        
+        // Obtener las uniform locations
         // Get the uniform locations
         GLint modelLoc = glGetUniformLocation(lightingShader.Program, "model");
         GLint viewLoc = glGetUniformLocation(lightingShader.Program, "view");
         GLint projLoc = glGetUniformLocation(lightingShader.Program, "projection");
 
+        // Pasar las matrices al shader
         // Pass the matrices to the shader
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
         model = glm::mat4(1);
         glBindVertexArray(0);
+
+        // Animación del fuego
+        // Contiene sus funciones en el shader Anim
 
         // Also draw the lamp object, again binding the appropriate shader
         lampshader.Use();
@@ -353,15 +445,17 @@ int main()
         Fuego.Draw(Anim);
         glBindVertexArray(0);
 
-        //Animación del caldo
+        // Animación del caldo
+        // Contiene sus funciones en el shader Anim2
+        
         // Also draw the lamp object, again binding the appropriate shader
         lampshader.Use();
         Anim2.Use();
         tiempo = glfwGetTime();
         // Get location objects for the matrices on the lamp shader (these could be different on a different shader)
-        modelLoc = glGetUniformLocation(Anim.Program, "model");
-        viewLoc = glGetUniformLocation(Anim.Program, "view");
-        projLoc = glGetUniformLocation(Anim.Program, "projection");
+        modelLoc = glGetUniformLocation(Anim2.Program, "model");
+        viewLoc = glGetUniformLocation(Anim2.Program, "view");
+        projLoc = glGetUniformLocation(Anim2.Program, "projection");
         // Set matrices
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
@@ -370,21 +464,23 @@ int main()
         model = glm::translate(model, glm::vec3(9.44f, 3.54f, 0.0f));
         model = glm::scale(model, glm::vec3(0.5f, 0.7f, 0.5f));
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-        glUniform1f(glGetUniformLocation(Anim.Program, "time"), tiempo);
+        glUniform1f(glGetUniformLocation(Anim2.Program, "time"), tiempo);
         Caldo.Draw(lampshader);
-        Caldo.Draw(Anim);
+        Caldo.Draw(Anim2);
         glBindVertexArray(0);
 
 
         //Animación de ingredientes
+        // Contiene sus funciones en el shader Anim3
+        
         // Also draw the lamp object, again binding the appropriate shader
         lampshader.Use();
         Anim3.Use();
         tiempo = glfwGetTime();
         // Get location objects for the matrices on the lamp shader (these could be different on a different shader)
-        modelLoc = glGetUniformLocation(Anim.Program, "model");
-        viewLoc = glGetUniformLocation(Anim.Program, "view");
-        projLoc = glGetUniformLocation(Anim.Program, "projection");
+        modelLoc = glGetUniformLocation(Anim3.Program, "model");
+        viewLoc = glGetUniformLocation(Anim3.Program, "view");
+        projLoc = glGetUniformLocation(Anim3.Program, "projection");
         // Set matrices
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
@@ -393,19 +489,15 @@ int main()
         model = glm::translate(model, glm::vec3(9.44f, 3.54f, 0.0f));
         model = glm::scale(model, glm::vec3(0.5f, 0.7f, 0.5f));
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-        glUniform1f(glGetUniformLocation(Anim.Program, "time"), tiempo);
+        glUniform1f(glGetUniformLocation(Anim3.Program, "time"), tiempo);
         Ingredientes.Draw(lampshader);
-        Ingredientes.Draw(Anim);
+        Ingredientes.Draw(Anim3);
         glBindVertexArray(0);
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture);
         lampshader.Use();
-        //glm::mat4 model(1);
-        /*glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
-        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
-        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
-        glBindVertexArray(VAO);*/
+
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
@@ -425,6 +517,7 @@ int main()
 // Moves/alters the camera positions based on user input
 void DoMovement()
 {
+    // Controles de la camara
     // Camera controls
     if (keys[GLFW_KEY_W] || keys[GLFW_KEY_UP])
     {
@@ -446,8 +539,13 @@ void DoMovement()
         camera.ProcessKeyboard(RIGHT, deltaTime);
     }
 
+/*
+>> Implementación de las animaciones sencillas
+>> Implementation of simple animations
+*/
 
-    //Animacion puerta principal
+    // Animacion puerta principal
+    // Consiste en abrir y cerrar la puerta, la cual se desliza de un lado a otro
     if (anim)
     {
         if (anim2)
@@ -471,7 +569,8 @@ void DoMovement()
 
     }
 
-    //Animacion puerta interior
+    // Animacion puerta interior
+    // Consiste en abrir y cerrar la puerta, la cual se desliza de un lado a otro
     if (anim3)
     {
         if (anim4)
@@ -496,6 +595,7 @@ void DoMovement()
     }
 
     //Animacion cajon armario
+    // Consiste en abrir y cerrar el cajon, la cual se desliza de un adentro hacia afuera
     if (anim5)
     {
         if (anim6)
@@ -519,7 +619,8 @@ void DoMovement()
 
     }
 
-    //Animacion puertas armario
+    // Animacion puertas armario
+    // Consiste en abrir y cerrar las puertas, las cuales se deslizan de un lado a otro
     if (anim7)
     {
         if (anim8)
@@ -563,18 +664,6 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode
         {
             keys[key] = false;
         }
-    }
-
-    if (keys[GLFW_KEY_O])
-    {
-        anim = true;
-        if (anim2)
-        {
-            anim2 = false;
-        }
-        else
-            anim2 = true;
-
     }
 
 
